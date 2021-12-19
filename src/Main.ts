73e0959441e4b53cb29ac9d6fe44da7bca74857f
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-12-19 11:48:18 -0300

    Change "Foxy Helper" to "Roxy"

--- src/Main.ts
+++ src/Main.ts
@@ -1,5 +1,5 @@
 import { Helper } from './client/RoxyClient';
 import * as Settings from '../settings.json';
 
-const FoxyHelper = new Helper({ intents: ["GUILDS", "DIRECT_MESSAGES", "GUILD_MESSAGES", "GUILD_MESSAGE_TYPING", "GUILD_MEMBERS"] }, Settings);
-FoxyHelper.start();
+const Roxy = new Helper({ intents: ["GUILDS", "DIRECT_MESSAGES", "GUILD_MESSAGES", "GUILD_MESSAGE_TYPING", "GUILD_MEMBERS"] }, Settings);
+Roxy.start();
