67b2f881baf19e13d4453556b1f200a5d9aca184
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2022-01-28 15:41:34 -0300

    Foxy radio yay

--- src/commands/NotificarCommand.ts
+++ src/commands/NotificarCommand.ts
@@ -48,10 +48,23 @@ export = {
                     message.member.roles.remove(role);
                     message.reply("<:sad_cat_1:768291156026720328> | Agora você não irá receber as atualizações de status da Foxy :3");
                 }
+                break;
+            }
+
+            case 'foxymix': {
+                const role = await message.guild.roles.fetch("936686433337499739");
+                if (!message.member.roles.cache.find(r => r.id === role.id)) {
+                    message.reply("<:meow_blush:768292358458179595> | Agora você irá receber as atualizações da rádio oficial da Foxy, confira aqui! https://zeno.fm/radio/foxymix/ :3");
+                    return message.member.roles.add(role);
+                } else {
+                    message.member.roles.remove(role);
+                    message.reply("<:sad_cat_1:768291156026720328> | Você não irá mais receber as atualizações da rádio oficial da Foxy :c");
+                }
+                break;
             }
 
             default: {
-                message.reply("<:meow_blush:768292358458179595> | Você deve informar o que deseja receber, `novidades`, `wip` ou `background`, `status`");
+                message.reply("<:meow_blush:768292358458179595> | Você deve informar o que deseja receber, `novidades`, `wip` ou `background`, `status`, `foxymix`");
             }
         }
     }
