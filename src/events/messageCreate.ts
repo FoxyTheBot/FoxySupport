4a4f16c34949a80c8617d02b40da16068742bfae
WinG4merBR <ddgomes2334@outlook.com>
2022-06-14 20:20:48 -0300

    Add a method for monthly donors to have premium without generating key

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,14 +1,22 @@
 import { Run } from '../client/Event';
-import * as faq from '../../faq.json';
 
 export const bind: string = 'messageCreate';
 
-export const run: Run = (client, message) => {
-    if(faq[message.content]) {
-        message.reply(faq[message.content]);
-    }
+export const run: Run = async (client, message) => {
+    const role = await message.guild.roles.fetch("986398691751522354");
+    const userRole = message.member.roles.cache.find(r => r.id === role.id);
 
-    const prefixRegex:Object = new RegExp(`^(${client.settings.prefix}|<@!?${client.user.id}>)( )*`, "gi");
+    if (userRole) {
+        const userData: any = await client.database.getUser(message.author.id);
+
+        if (userData.premium === false) {
+            userData.premium = true;
+            userData.premiumDate = new Date();
+            userData.save();
+            console.info(`Premium activated for ${message.author.id}`);
+        }
+    }
+    const prefixRegex: Object = new RegExp(`^(${client.settings.prefix}|<@!?${client.user.id}>)( )*`, "gi");
 
     if (!message.content.match(prefixRegex)) return;
 
@@ -16,11 +24,11 @@ export const run: Run = (client, message) => {
     const commandName = args.shift().toLowerCase();
 
     const command = client.commands.get(commandName);
-    if(!command) return;
+    if (!command) return;
 
     try {
         command.run(client, message, args);
-    } catch(error) {
+    } catch (error) {
         console.error(error);
     }
 }
