87e7d01f228756972ebedb349f79eac4108dc468
WinG4merBR <ddgomes2334@outlook.com>
2022-06-15 17:26:11 -0300

    Trying to fix 404 errors

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -7,41 +7,43 @@ export const run: Run = async (client, message) => {
     const roles = ["986676570431307827", "986676477833642024", "986672822690676778"]
     const userData: any = await client.database.getUser(message.author.id);
 
-    if (userData.premium && !userData.premiumType) {
-        const role = await message.guild.roles.fetch("986710043657400321");
-        if (!message.member.roles.cache.find(r => r.id === role.id)) {
-            message.member.roles.add(role);
+    if (userData) {
+        if (userData.premium && !userData.premiumType) {
+            const role = await message.guild.roles.fetch("986710043657400321");
+            if (!message.member.roles.cache.find(r => r.id === role.id)) {
+                message.member.roles.add(role);
+            }
+            userData.premiumType = "VETERAN";
+            userData.save();
         }
-        userData.premiumType = "VETERAN";
-        userData.save();
-    }
 
-    for (const role of roles) {
-        const roleObj = await message.guild.roles.fetch(role);
-        if (message.member.roles.cache.find(r => r.id === roleObj.id)) {
-            switch (role) {
-                case "986676570431307827": {
-                    userData.premiumType = "INFINITY_TURBO";
-                    userData.premium = true;
-                    userData.premiumDate = new Date();
-                    userData.save();
-                    break;
-                }
+        for (const role of roles) {
+            const roleObj = await message.guild.roles.fetch(role);
+            if (message.member.roles.cache.find(r => r.id === roleObj.id)) {
+                switch (role) {
+                    case "986676570431307827": {
+                        userData.premiumType = "INFINITY_TURBO";
+                        userData.premium = true;
+                        userData.premiumDate = new Date();
+                        userData.save();
+                        break;
+                    }
 
-                case "986676477833642024": {
-                    userData.premiumType = "INFINITY_PRO";
-                    userData.premium = true;
-                    userData.premiumDate = new Date();
-                    userData.save();
-                    break;
-                }
+                    case "986676477833642024": {
+                        userData.premiumType = "INFINITY_PRO";
+                        userData.premium = true;
+                        userData.premiumDate = new Date();
+                        userData.save();
+                        break;
+                    }
 
-                case "986672822690676778": {
-                    userData.premiumType = "INFINITY_ESSENTIALS";
-                    userData.premium = true;
-                    userData.premiumDate = new Date();
-                    userData.save();
-                    break;
+                    case "986672822690676778": {
+                        userData.premiumType = "INFINITY_ESSENTIALS";
+                        userData.premium = true;
+                        userData.premiumDate = new Date();
+                        userData.save();
+                        break;
+                    }
                 }
             }
         }
