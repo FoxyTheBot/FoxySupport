e2d22ad4fc8c0410a6c5b1d8814510c30a71f10d
WinG4merBR <ddgomes2334@outlook.com>
2022-07-11 21:21:16 -0300

    Add webhook checker

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -5,60 +5,61 @@ export const bind: string = 'messageCreate';
 export const run: Run = async (client, message) => {
     /* PremiumType checker */
     const roles = ["986676570431307827", "986676477833642024", "986672822690676778"]
-    const userData: any = await client.database.getUser(message.author.id);
-
-    if (userData) {
-        if (userData.premium && !userData.premiumType) {
-            userData.premiumType = "VETERAN";
-            userData.save();
-        }
-        if (userData.premium && userData.premiumType === "VETERAN") {
-            const role = await message.guild.roles.fetch("986710043657400321");
-            if (!message.member.roles.cache.find(r => r.id === role.id)) {
-                message.member.roles.add(role);
-            }
-        }
-
-        for (const role of roles) {
-            const roleObj = await message.guild.roles.fetch(role);
-            if (message.member.roles.cache.find(r => r.id === roleObj.id)) {
-                switch (role) {
-                    case "986676570431307827": {
-                        userData.balance += 50000;
-                        userData.premiumType = "INFINITY_TURBO";
-                        userData.premium = true;
-                        userData.premiumDate = new Date();
-                        userData.save();
-                        break;
-                    }
-
-                    case "986676477833642024": {
-                        userData.balance += 25000;
-                        userData.premiumType = "INFINITY_PRO";
-                        userData.premium = true;
-                        userData.premiumDate = new Date();
-                        userData.save();
-                        break;
-                    }
-
-                    case "986672822690676778": {
-                        userData.balance += 10000;
-                        userData.premiumType = "INFINITY_ESSENTIALS";
-                        userData.premium = true;
-                        userData.premiumDate = new Date();
-                        userData.save();
-                        break;
-                    }
-                }
-            } else {
-                userData.premiumType = null;
-                userData.premium = false;
-                userData.premiumDate = null;
+    if (!message.isWebhook) {
+        const userData: any = await client.database.getUser(message.author.id);
+        if (userData) {
+            if (userData.premium && !userData.premiumType) {
+                userData.premiumType = "VETERAN";
                 userData.save();
             }
-        }
-    }
+            if (userData.premium && userData.premiumType === "VETERAN") {
+                const role = await message.guild.roles.fetch("986710043657400321");
+                if (!message.member.roles.cache.find(r => r.id === role.id)) {
+                    message.member.roles.add(role);
+                }
+            }
 
+            for (const role of roles) {
+                const roleObj = await message.guild.roles.fetch(role);
+                if (message.member.roles.cache.find(r => r.id === roleObj.id)) {
+                    switch (role) {
+                        case "986676570431307827": {
+                            userData.balance += 50000;
+                            userData.premiumType = "INFINITY_TURBO";
+                            userData.premium = true;
+                            userData.premiumDate = new Date();
+                            userData.save();
+                            break;
+                        }
+
+                        case "986676477833642024": {
+                            userData.balance += 25000;
+                            userData.premiumType = "INFINITY_PRO";
+                            userData.premium = true;
+                            userData.premiumDate = new Date();
+                            userData.save();
+                            break;
+                        }
+
+                        case "986672822690676778": {
+                            userData.balance += 10000;
+                            userData.premiumType = "INFINITY_ESSENTIALS";
+                            userData.premium = true;
+                            userData.premiumDate = new Date();
+                            userData.save();
+                            break;
+                        }
+                    }
+                } else {
+                    userData.premiumType = null;
+                    userData.premium = false;
+                    userData.premiumDate = null;
+                    userData.save();
+                }
+            }
+        }
+
+    }
     /* End of PremiumType checker */
 
     const prefixRegex: Object = new RegExp(`^(${client.settings.prefix}|<@!?${client.user.id}>)( )*`, "gi");
