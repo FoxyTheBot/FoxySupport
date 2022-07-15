558ba75808d267b19cffe4536c86ff7a298b569c
WinG4merBR <ddgomes2334@outlook.com>
2022-07-15 09:19:53 -0300

    Remove a presence

--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -5,7 +5,6 @@ export const bind: string = 'ready';
 export const run: Run = (client) => {
     console.log("\x1b[37m\x1b[44mINFO\x1b[0m: Roxy está online!");
     let status: object[] = [
-        { name: `Roberto Carlos - Guerra dos Meninos`, type: "LISTENING" },
         { name: `The Living Tombstone - It's been so long`, type: "LISTENING" },
         { name: "Undertale - Hopes and Dreams", type: "LISTENING" },
     ];
