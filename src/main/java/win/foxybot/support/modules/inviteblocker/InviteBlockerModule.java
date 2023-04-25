49efcfc27571bc3300280d7b181ca4b7d9347a85
WinG4merBR <wingamerbr@foxybot.win>
2023-04-25 14:11:11 -0300

    Create InviteBlockerModule to block Discord Servers invite

---
+++ src/main/java/win/foxybot/support/modules/inviteblocker/InviteBlockerModule.java
@@ -0,0 +1,25 @@
+package win.foxybot.support.modules.inviteblocker;
+
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import win.foxybot.support.utils.Emotes;
+
+import java.util.Objects;
+
+public class InviteBlockerModule extends ListenerAdapter {
+    public void onMessageReceived(MessageReceivedEvent e) {
+        Message message = e.getMessage();
+        String messageContent = e.getMessage().getContentRaw();
+        long guildId = e.getGuild().getIdLong();
+        long channelId = e.getChannel().getIdLong();
+
+        if (guildId != 768267522670723094L || channelId != 869384482552152104L) return;
+        if (messageContent.contains("discord.gg")
+                || messageContent.contains("discord.com/invite")
+                || messageContent.contains("discordapp.com/invite")) {
+            message.delete().queue();
+            e.getChannel().sendMessage(Emotes.FOXY_BAN + Objects.requireNonNull(e.getMember()).getAsMention() + " Você não pode enviar convites de servidores aqui!").queue();
+        }
+    }
+}
