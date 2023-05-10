a967cf25bff8164655e5fc5a383e5091c397018d
WinG4merBR <win4merbr@foxybot.win>
2023-05-09 21:34:53 -0300

    Add `GuildMemberAdd` in `setupEventHandlers`

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,7 +1,9 @@
+import { setGuildMemberAddEvent } from "./handler/guildMemberAdd";
 import { setReadyEvent } from "./handler/ready";
 
 const setupEventHandlers = (): void => {
     setReadyEvent();
+    setGuildMemberAddEvent();
 }
 
 export { setupEventHandlers }
