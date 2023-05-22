e792274955a98f451d6ab2dc4588d356728ccf98
WinG4merBR <wingamerbr@foxybot.win>
2023-05-22 16:04:32 -0300

    Remove `Welcome` module

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,9 +1,7 @@
-import { setGuildMemberAddEvent } from "./handler/guildMemberAdd";
 import { setReadyEvent } from "./handler/ready";
 
 const setupEventHandlers = (): void => {
     setReadyEvent();
-    setGuildMemberAddEvent();
 }
 
 export { setupEventHandlers }
