f6e17dc3929eb96af3b935f867521fa0f4064ade
WinG4merBR <wingamerbr@foxybot.win>
2024-06-02 19:31:36 -0300

    Add `globalExecutions` property

--- src/structures/types/foxy.ts
+++ src/structures/types/foxy.ts
@@ -3,6 +3,7 @@ import { ChatInputInteractionCommand } from './command';
 import { botHasGuildPermissions } from 'discordeno/permissions-plugin';
 import { BotWithCache } from 'discordeno/cache-plugin';
 import DatabaseConnection from '../database/DatabaseConnection';
+import ComponentInteractionContext from '../../command/structures/ComponentInteractionContext';
 export interface IdentifiedData<T> {
   id: number;
   data: T;
@@ -19,4 +20,5 @@ export interface FoxyClient extends BotWithCache<Bot> {
   locale: Function;
   isReady: boolean;
   hasGuildPermission: typeof botHasGuildPermissions;
+  globalExecutions: ((context: ComponentInteractionContext<any>) => Promise<unknown>)[];
 }
